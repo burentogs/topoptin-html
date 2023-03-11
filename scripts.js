f1affd8de829f63bbc2dd418bb0991c749908416
Burentugs <j.burentogs@gmail.com>
2023-03-11 12:32:22 +0800

    Final

---
+++ scripts.js
@@ -0,0 +1,71 @@
+var transnav = document.getElementById("header");
+
+window.onscroll = function () {
+  toggleOpaque();
+};
+
+function toggleOpaque() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    transnav.classList.add("opaque");
+  } else {
+    transnav.classList.remove("opaque");
+  }
+}
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+function copyFunction(string) {
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(string);
+
+  // Alert the copied text
+  alert("Copied the text: " + string);
+}
+
+var themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
+var themeToggleLightIcon = document.getElementById("theme-toggle-light-icon");
+
+// Change the icons inside the button based on previous settings
+if (
+  localStorage.getItem("color-theme") === "dark" ||
+  (!("color-theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  themeToggleLightIcon.classList.remove("hidden");
+} else {
+  themeToggleDarkIcon.classList.remove("hidden");
+}
+
+var themeToggleBtn = document.getElementById("theme-toggle");
+
+themeToggleBtn.addEventListener("click", function () {
+  // toggle icons inside button
+  themeToggleDarkIcon.classList.toggle("hidden");
+  themeToggleLightIcon.classList.toggle("hidden");
+
+  // if set via local storage previously
+  if (localStorage.getItem("color-theme")) {
+    if (localStorage.getItem("color-theme") === "light") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    }
+
+    // if NOT set via local storage previously
+  } else {
+    if (document.documentElement.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("color-theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("color-theme", "dark");
+    }
+  }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00000",
+        secondary: "#7c7c7c",
+        tertiary: "#dcdcdc",
+        quaternary: "#F6F6F6",
+        light_topoptin: "#38B6FF",
+        topoptin: "#0c45b5",
+        footer_black: "#111",
+        border_black: "#333",
+      },
+    },
+  },
+  plugins: [],
+};
